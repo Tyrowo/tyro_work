8977d0f71701924ad57a10ccea0042603a147285
Tyrowo <tycrews64@gmail.com>
2023-11-07 21:22:41 -0500

    switchesAndStorage - got switches to update state of parent by passing a whole function into the child (screen) class. Pretty sick. Local storage and switches are working as intended.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tyro_work/helper/themes.dart';
+import 'package:tyro_work/screens/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +17,18 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool tyFontOff = false;
   bool darkMode = false;
+
+  void _updateFont(bool tyOff) async {
+    setState(() => tyFontOff = tyOff);
+    final localStorage = await SharedPreferences.getInstance();
+    localStorage.setBool('tyFontOff', tyOff);
+  }
+
+  void _updateLight(bool darkOn) async {
+    setState(() => darkMode = darkOn);
+    final localStorage = await SharedPreferences.getInstance();
+    localStorage.setBool('darkModeOn', darkOn);
+  }
 
   Future<void> checkPrefsLight() async {
     final localStorage = await SharedPreferences.getInstance();
@@ -44,174 +55,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Tyro Work',
       theme: darkMode ? darkTheme(tyFontOff) : lightTheme(tyFontOff),
-      home: const MyHomePage(title: 'Tyro Work'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _background = 4;
-  bool tyFontOff = false;
-  bool darkMode = false;
-
-  void _randomizeBackground() {
-    setState(() {
-      _background = Random().nextInt(5) + 1;
-    });
-  }
-
-  Future<void> checkPrefsLight() async {
-    final localStorage = await SharedPreferences.getInstance();
-    bool darkModeOn = localStorage.getBool('darkModeOn') ?? false;
-    setState(() => darkMode = darkModeOn);
-  }
-
-  Future<void> checkPrefsFont() async {
-    final localStorage = await SharedPreferences.getInstance();
-    bool tyFont = localStorage.getBool('tyFontOff') ?? false;
-    setState(() => tyFontOff = tyFont);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    checkPrefsFont();
-    checkPrefsLight();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-          clipBehavior: Clip.antiAlias,
-          leading: Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
-            child: Image.asset('assets/ty48.png'),
-          ),
-          title: Row(
-            // crossAxisAlignment: WrapCrossAlignment.center,
-            children: [
-              const Text('TyRowo',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Tyrowo Inked',
-                      fontWeight: FontWeight.w600)),
-              const Spacer(),
-              const SizedBox(width: 10),
-              ElevatedButton(onPressed: () {}, child: const Text('Resume')),
-              const SizedBox(width: 10),
-              ElevatedButton(onPressed: () {}, child: const Text('Contact')),
-              const SizedBox(width: 20),
-              SizedBox(
-                width: 65,
-                height: 50,
-                child: FittedBox(
-                  fit: BoxFit.fill,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text('Ty',
-                              style: TextStyle(
-                                  fontFamily: 'Tyrowo Inked',
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18)),
-                          Switch(
-                            value: tyFontOff,
-                            onChanged: (bool val) async {
-                              final localStorage =
-                                  await SharedPreferences.getInstance();
-                              localStorage.setBool('tyFontOff', val);
-                              setState(() => tyFontOff = val);
-                            },
-                          ),
-                          const Text('Ø ',
-                              // capital Ø, lowercase ø // need a space after to balance out with the ty on the other side
-                              style: TextStyle(
-                                  fontFamily: 'Ubuntu', fontSize: 18)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Icon(Icons.light_mode_outlined),
-                          Switch(
-                              value: darkMode,
-                              onChanged: (bool val) async {
-                                final localStorage =
-                                    await SharedPreferences.getInstance();
-                                localStorage.setBool('darkModeOn', val);
-                                setState(() => tyFontOff = val);
-                              }),
-                          const Icon(Icons.dark_mode_outlined),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          )),
-      body: Center(
-        child: Stack(
-          children: [
-            FractionallySizedBox(
-              widthFactor: 1.0,
-              heightFactor: 1.0,
-              child: Positioned.fill(
-                child: Image.asset(
-                  'assets/$_background.png',
-                  repeat: ImageRepeat.repeat,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 80, 0, 40),
-              child: Center(
-                child: Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'You have pushed the button this many times:',
-                      ),
-                      Text(
-                        '$_background',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      ElevatedButton(
-                        child: const Text('hi'),
-                        onPressed: () => print('hi'),
-                      ),
-                      TextButton(
-                        child: const Text('a link'),
-                        onPressed: () => print('ok'),
-                      ),
-                      const Icon(Icons.abc_outlined),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _randomizeBackground,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: Homescreen(updateLight: _updateLight, updateFont: _updateFont),
     );
   }
 }
---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,168 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Homescreen extends StatefulWidget {
+  final ValueChanged<bool> updateFont;
+  final ValueChanged<bool> updateLight;
+  const Homescreen(
+      {super.key, required this.updateFont, required this.updateLight});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  int _background = 4;
+  bool tyFontOff = false;
+  bool darkMode = false;
+
+  void _randomizeBackground() {
+    setState(() {
+      _background = Random().nextInt(5) + 1;
+    });
+  }
+
+  Future<void> checkPrefsLight() async {
+    final localStorage = await SharedPreferences.getInstance();
+    bool darkModeOn = localStorage.getBool('darkModeOn') ?? false;
+    setState(() => darkMode = darkModeOn);
+  }
+
+  Future<void> checkPrefsFont() async {
+    final localStorage = await SharedPreferences.getInstance();
+    bool tyFont = localStorage.getBool('tyFontOff') ?? false;
+    setState(() => tyFontOff = tyFont);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkPrefsFont();
+    checkPrefsLight();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          clipBehavior: Clip.antiAlias,
+          leading: Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+            child: Image.asset('assets/ty48.png'),
+          ),
+          title: Row(
+            // crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              const Text('TyRowo',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Tyrowo Inked',
+                      fontWeight: FontWeight.w600)),
+              const Spacer(),
+              const SizedBox(width: 10),
+              ElevatedButton(onPressed: () {}, child: const Text('Resume')),
+              const SizedBox(width: 10),
+              ElevatedButton(onPressed: () {}, child: const Text('Contact')),
+              const SizedBox(width: 20),
+              SizedBox(
+                width: 65,
+                height: 50,
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text('Ty',
+                              style: TextStyle(
+                                  fontFamily: 'Tyrowo Inked',
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18)),
+                          Switch(
+                            value: tyFontOff,
+                            onChanged: (bool val) async {
+                              widget.updateFont(val);
+                              setState(() => tyFontOff = val);
+                            },
+                          ),
+                          const Text('Ø ',
+                              // capital Ø, lowercase ø // need a space after to balance out with the ty on the other side
+                              style: TextStyle(
+                                  fontFamily: 'Ubuntu', fontSize: 18)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.light_mode_outlined),
+                          Switch(
+                              value: darkMode,
+                              onChanged: (bool val) async {
+                                widget.updateLight(val);
+                                setState(() => darkMode = val);
+                              }),
+                          const Icon(Icons.dark_mode_outlined),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+      body: Center(
+        child: Stack(
+          children: [
+            FractionallySizedBox(
+              widthFactor: 1.0,
+              heightFactor: 1.0,
+              child: Positioned.fill(
+                child: Image.asset(
+                  'assets/$_background.png',
+                  repeat: ImageRepeat.repeat,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 80, 0, 40),
+              child: Center(
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'You have pushed the button this many times:',
+                      ),
+                      Text(
+                        '$_background',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      ElevatedButton(
+                        child: const Text('hi'),
+                        onPressed: () => print('hi'),
+                      ),
+                      TextButton(
+                        child: const Text('a link'),
+                        onPressed: () => print('ok'),
+                      ),
+                      const Icon(Icons.abc_outlined),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _randomizeBackground,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
