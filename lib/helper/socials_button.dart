5297786c9e25c03f606b72a9161bc3928209b6fb
Tyrowo <tycrews64@gmail.com>
2023-11-10 16:06:06 -0500

    thoughtsAndContacts - wrapped icons in a round textlink to get that cool consistent styling i worked on

--- lib/helper/socials_button.dart
+++ lib/helper/socials_button.dart
@@ -9,14 +9,21 @@ class SocialsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: SizedBox(
-        height: 36,
-        width: 36,
-        child: Image.asset(
-          image,
-          fit: BoxFit.scaleDown,
+    return TextButton(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all(const CircleBorder()),
+          padding: MaterialStateProperty.all(EdgeInsets.zero)),
+      // luckily our text button is already styled but we add a nice little circle radius to it
+      onPressed: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SizedBox(
+          height: 36,
+          width: 36,
+          child: Image.asset(
+            image,
+            fit: BoxFit.scaleDown,
+          ),
         ),
       ),
     );
