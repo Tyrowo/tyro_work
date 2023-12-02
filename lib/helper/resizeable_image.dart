61ce985628ed95ffdb573719e3d9df009a6f17d0
Tyrowo <tycrews64@gmail.com>
2023-12-02 02:36:41 -0500

    improvements - worked hard to get the button to close out of full screen to be a little better positioned. After fighting the math for a couple hours this is the best I've3 got.

--- lib/helper/resizeable_image.dart
+++ lib/helper/resizeable_image.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tyro_work/helper/themes.dart';
 
 class ResizeableImage extends StatefulWidget {
   final String asset;
@@ -35,10 +36,9 @@ class _ResizeableImageState extends State<ResizeableImage> {
         ],
       ),
       constraints: BoxConstraints(
-          maxHeight: deviceHeight(context) * (1 / (widget.imagesInWrap + 1)),
-          maxWidth: deviceWidth(context) * (1 / (widget.imagesInWrap + 1))),
-      // height: deviceHeight(context) * (1 / (widget.imagesInWrap)),
-      // width: deviceWidth(context) * (1 / (widget.imagesInWrap + 1)),
+        maxHeight: deviceHeight(context) * (1 / (widget.imagesInWrap + 1)),
+        maxWidth: deviceWidth(context) * (1 / (widget.imagesInWrap + 1)),
+      ),
       child: GestureDetector(
           onTap: () => showPicFullscreen(context),
           child: Image.asset(
@@ -57,6 +57,7 @@ class _ResizeableImageState extends State<ResizeableImage> {
     List<double> picDimensions = await getPicSize(widget.asset);
     double picHeight = picDimensions[0];
     double picWidth = picDimensions[1];
+    bool widerThanTall = picWidth > picHeight;
     // cannot use buildcontext across async gaps requires this mounted check, which requires the whole widget to be stateful
     if (mounted) {
       return await showDialog(
@@ -66,19 +67,51 @@ class _ResizeableImageState extends State<ResizeableImage> {
           shadowColor: const Color.fromRGBO(100, 100, 100, 0),
           backgroundColor: const Color.fromRGBO(100, 100, 100, 0),
           surfaceTintColor: const Color.fromRGBO(100, 100, 100, 0),
-          child: SizedBox(
-            height: min(deviceHeight(context) * 0.95, picHeight + 75),
-            width: min(deviceWidth(context) * 0.95, picWidth + 75),
+          child: Container(
+            // when side x is longer than side y, and pic is x by y
+            // the longer side will take the full size of the screen
+            // so longerSide = deviceSize(context)
+            // and shorterSide = (shorterSide / longerSide) * deviceSize(context)
+            // just use the same device size of the longer for BOTH
+            // but to account for the pics that are smaller than the screen, we take the min of the larger picSize, deviceSize
+
+            // this works almost perfect, but encounters an issue when the image is wider on one side but close to square so you get clamped by the other side first so the ratio is wrong
+            // also to explain the 75s - when the image is smaller than the container size, the 75s are the perfect offset to finesse the button to hang over the corner nicely
+            constraints: BoxConstraints(
+              maxWidth: (widerThanTall
+                      ? min(picWidth, deviceWidth(context) * 0.9)
+                      : (picWidth / picHeight) *
+                          min(picHeight, deviceHeight(context) * 0.9)) +
+                  75,
+              maxHeight: (!widerThanTall
+                      ? min(picHeight, deviceHeight(context) * 0.9)
+                      : (picHeight / picWidth) *
+                          min(picWidth, deviceWidth(context) * 0.9)) +
+                  75,
+            ),
+            /*
+            for debugging this math problem only
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red,
+                  blurRadius: 4,
+                  offset: Offset(10, 15),
+                )
+              ],
+            ),
+            */
             child: Stack(
               alignment: AlignmentDirectional.topEnd,
               children: <Widget>[
                 Center(
                     child: InteractiveViewer(
+                        trackpadScrollCausesScale: true,
+                        clipBehavior: Clip.none,
                         child: Image.asset(
-                  widget.asset,
-                  semanticLabel: widget.label,
-                  fit: BoxFit.cover,
-                ))),
+                          widget.asset,
+                          semanticLabel: widget.label,
+                        ))),
                 IconButton(
                   onPressed: () {
                     Navigator.pop(context);
